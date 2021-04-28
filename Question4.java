6847f22e65fb27942e5d5770de900348ed50446d
joostinn <73330036+joostinn@users.noreply.github.com>
2021-04-28 09:17:00 +0800

    completed question 4

--- Question4.java
+++ Question4.java
@@ -20,7 +20,7 @@ public class Question4
     int rows = in.nextInt();
     for (int i = rows; i >= 1; --i) {
       for (int j = 1; j <= i; ++j) {
-        System.out.print("* ");
+        System.out.print("*");
       }
       System.out.println();
     }
